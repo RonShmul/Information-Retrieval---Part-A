a408a052debb668cc38c67f552e6664369df7acf
Sivan <sv.reje@gmail.com>
2017-11-28 20:27:12 +0200

    function percent in Parse class

--- src/Parse.java
+++ src/Parse.java
@@ -2,5 +2,19 @@
  * Created by Ronshmul on 28/11/2017.
  */
 public class Parse {
-
+    public String percent(String str) {
+        if(str.contains("percentage")) {
+            int index = str.indexOf("percentage");
+            String result = numbers(str.substring(0, index - 1)) + " percent";
+            return result;
+        }
+        else if(str.contains("%")) {
+            int index = str.indexOf("%");
+            String result = numbers(str.substring(0, index - 1)) + " percent";
+            return result;
+        }
+        else {
+            return str;
+        }
+    }
 }
