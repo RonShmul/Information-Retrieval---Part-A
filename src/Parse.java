8421af02ef93c8eae76e8e86f0b282529c566a2f
Ronshmul <ronush.92@gmail.com>
2017-11-28 20:25:00 +0200

    create numbers function

--- src/Parse.java
+++ src/Parse.java
@@ -3,4 +3,23 @@
  */
 public class Parse {
 
+    public String numbers(String str){
+        if(str.contains(".")) {
+            String[] parts = str.split(".");
+            String str1 = parts[1];
+            if (str1.length() > 2) {
+                int digit = Integer.parseInt(str1.substring(1,1))+1;
+                str = parts[0]+ "." + digit;
+            }
+        }
+        if(str.contains(",")){
+            String[] parts = str.split(",");
+            String str1= "";
+            for (int i=0; i<parts.length; i++){
+                str1+=parts[i];
+            }
+        }
+        return str;
+    }
+
 }
