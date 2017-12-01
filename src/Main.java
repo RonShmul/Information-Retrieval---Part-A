2e26aa84eb9dd07911f16d2b147da57f4ebc7e6b
Ronshmul <ronush.92@gmail.com>
2017-12-01 11:45:21 +0200

    create numbers function

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,8 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
 /**
  * Created by Ronshmul on 28/11/2017.
  */
@@ -13,8 +18,27 @@ public class Main {
         String y1 = Parse.numbers(y);
 //        String z1 = Parse.numbers(z);
 //        System.out.println(x1);
-        System.out.println(y1);
+//        System.out.println(y1);
 //        System.out.println(z1);
+        try {
+            File file = new File("C:\\Users\\Ronshmul\\IdeaProjects\\Information-Retrieval---Part-A\\src\\ahlaDoc");
+            Scanner scan = new Scanner(file);
+
+            scan.useDelimiter(Pattern.compile("<DOCNO>"));
+            String line = scan.next();
+            System.out.println(line);
+
+            scan.useDelimiter(Pattern.compile("</DOCNO>"));
+            line = scan.next();
+            System.out.println(line);
+
+               /* BufferedReader reader = new BufferedReader(new FileReader(currFile));
+                String line; */
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
     }
 }
 
