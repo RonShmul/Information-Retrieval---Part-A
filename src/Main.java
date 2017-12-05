0dca9ff6fce3c39314da647e90e309b0e9ec2751
Sivan <sv.reje@gmail.com>
2017-12-05 18:46:57 +0200

    buffered reader

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.Scanner;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 /**
@@ -12,12 +13,33 @@ import java.util.regex.Pattern;
 
 public class Main {
     public static void main(String [] args) {
-        ReadFile readFile = new ReadFile();
-        try {
-            readFile.readDocs();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+//        ReadFile readFile = new ReadFile();
+//        try {
+//            readFile.readDocs();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+
+
+//        Pattern dots = Pattern.compile("[A-Z]\\w*");
+//
+//        Matcher docnoMatcher = dots.matcher("Sivan");
+//        if(docnoMatcher.find()) {
+//            String docno = docnoMatcher.group();
+//            System.out.println(docno);
+//            String s = "(3fgh) 'erg: h[;rtr]' \"rt: rg\" {e;rtert}".replaceAll("[\\[\\](){}]|\"|:|;","");
+//                    //.replaceAll("[\\[\\](){}]","").replaceAll("\"","");
+//            System.out.println(s);
+//        }
+//        else
+//            System.out.println("basa");
+
+
+        FileRed fileRed = new FileRed();
+        fileRed.setPathStr("D:\\Sivan\\studies\\corpus");
+        fileRed.setListOfDirs();
+        fileRed.readCorpus();
     }
+
 }
 
